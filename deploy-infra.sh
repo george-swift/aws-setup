d7d908e37ff0077cb4131f86fbcaf7ab808963c6
George <71434803+george-swift@users.noreply.github.com>
2021-06-04 16:11:12 +0100

    Add CodeDeploy/CodeBuild/CodePipeline

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -7,17 +7,26 @@ CLI_PROFILE=awssetup
 EC2_INSTANCE_TYPE=t2.micro
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awssetup --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-setup-access-token)
+GH_OWNER=$(cat ~/.github/aws-setup-owner)
+GH_REPO=$(cat ~/.github/aws-setup-repo)
+GH_BRANCH=master
 
 # Deploy the CloudFormation template
-echo -e "\n\n========== Deploying setup.yml ============="
+echo -e "\n\n========== Deploying main.yml ============="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
-  --template-file setup.yml \
+  --template-file main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
+    EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
  # If the deploy succeeded, show the DNS name of the created instance
